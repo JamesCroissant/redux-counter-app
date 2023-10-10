f1e12a065eef16d672b54be4695c9276d28d6454
Yu Hamada <yuham1014@gmail.com>
2023-10-10 11:51:18 -0700

    update to rename in counterSlice.js

--- redux-counter-app/src/redux/counterSlice.js
+++ redux-counter-app/src/redux/counterSlice.js
@@ -1,6 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
 
-export const counterSlicer = createSlice({
+export const counterSlice = createSlice({
   name: "counter",
   initialState: {
     value: 0,
