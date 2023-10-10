afa7d1242883172843d9448cd18c13cd2d0ab2c2
Yu Hamada <yuham1014@gmail.com>
2023-10-10 11:59:18 -0700

    update to setup increment and decrement in App.js

--- redux-counter-app/src/App.js
+++ redux-counter-app/src/App.js
@@ -1,8 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
+import { decrement, increment } from './redux/counterSlice';
 
 function App() {
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
   return (
     <div className="App">
+      <h2>count : {count}</h2>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <button onClick={() => dispatch(decrement())}>-</button>
     </div>
   );
 }
