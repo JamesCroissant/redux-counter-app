149d589fc9f20e2f5effc4953f9d13b7c716967f
Yu Hamada <yuham1014@gmail.com>
2023-10-10 12:20:08 -0700

    update to setup incrementByAmount in App.js

--- redux-counter-app/src/App.js
+++ redux-counter-app/src/App.js
@@ -1,16 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
-import { decrement, increment } from './redux/counterSlice';
+import { decrement, increment, incrementByAmount } from './redux/counterSlice';
+import { useState } from 'react';
 
 function App() {
   const count = useSelector((state) => state.counter.value);
   const dispatch = useDispatch();
 
+  const [incrementAmount, setIncrementAmount] = useState("");
+
   return (
     <div className="App">
       <h2>count : {count}</h2>
+      <input onChange={(e) => setIncrementAmount(e.target.value)} value={incrementAmount} />
       <button onClick={() => dispatch(increment())}>+</button>
       <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(incrementByAmount(parseInt(incrementAmount)))}>add</button>
     </div>
   );
 }
